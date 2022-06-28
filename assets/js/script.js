f5888f9bde784db8040beb330b737957ce97e867
kazcare <kazcare@hotmail.com>
2022-06-28 21:42:02 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,17 +14,28 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
+    runGame("addition");
+
 });
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+ function runGame(gameType) {
 
-    //Create two random number between 1 and 30
-    let num1 = Math.floor(Math.random() * 30)+1;
-    let num2 = Math.floor(Math.random() * 30)+1;
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -43,8 +54,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
